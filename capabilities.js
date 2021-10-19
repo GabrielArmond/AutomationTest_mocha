6fbf5f7249c7f2adc194203eaee512720c2cb003
Gabriel Armond <gabriel.armond77@gmail.com>
2021-10-19 11:30:46 -0300

    first commit

---
+++ capabilities.js
@@ -0,0 +1,11 @@
+var capabilities = {
+  build: 'Test Build',
+  name: 'Selenium Test',
+  platform: 'Windows 10',
+  browserName: 'Chrome',
+  version: '95.0'
+}
+
+module.exports = {
+  capabilities
+}
---
+++ test/firstTest.js
@@ -0,0 +1,45 @@
+const { Builder, By, Key } = require('selenium-webdriver')
+var chrome = require('Selenium-webdriver/chrome')
+const { it } = require('mocha')
+var should = require('chai').should()
+
+require('selenium-webdriver/chrome')
+require('chromedriver')
+
+//describe block
+describe('add todo tests', function () {
+  var driver
+
+  beforeEach(() => {
+    driver = new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new chrome.Options().addArguments('--headless'))
+      .build()
+  })
+
+  afterEach(async () => {
+    await driver.quit()
+  })
+
+  //it block
+  it('successfully add todo to application', async () => {
+    //naviagete to our application
+    await driver.get('https://lambdatest.github.io/sample-todo-app/')
+
+    //add a search
+    await driver
+      .findElement(By.id('sampletodotext'))
+      .sendKeys('Learn Selenium', Key.RETURN)
+
+    //assert
+    let todoText = await driver
+      .findElement(By.xpath('//li[last()]'))
+      .getText()
+      .then(function (value) {
+        return value
+      })
+
+    //assert using chai should
+    todoText.should.equal('Learn Selenium')
+  })
+})
